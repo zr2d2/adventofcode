bb6950ebe763ddf72b9aff98f3ded814a06e6ab2
Zach Rowe <zrowe007@gmail.com>
2019-12-08 00:21:28 -0500

    day 2 part 2

--- 2019/day2.rb
+++ 2019/day2.rb
@@ -1,11 +1,11 @@
 input = '1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,1,19,6,23,2,23,13,27,1,27,5,31,2,31,10,35,1,9,35,39,1,39,9,43,2,9,43,47,1,5,47,51,2,13,51,55,1,55,9,59,2,6,59,63,1,63,5,67,1,10,67,71,1,71,10,75,2,75,13,79,2,79,13,83,1,5,83,87,1,87,6,91,2,91,13,95,1,5,95,99,1,99,2,103,1,103,6,0,99,2,14,0,0'.split(',')
 
-def puzzle(inputs)
+def puzzle(inputs,noun,verb)
     answer = inputs.dup
 
-    answer[1] = 12
+    answer[1] = noun
 
-    answer[2] = 2
+    answer[2] = verb
 
     answer.map!{ |e| e = e.to_i }
 
@@ -31,6 +31,16 @@ def puzzle(inputs)
     end
 end
 
-results = puzzle input
+limit = 100
 
-puts results[0]
+(0..limit).each do |noun|
+    (0..limit).each do |verb|
+        results = puzzle input, noun, verb
+        
+        if results[0] == 19690720
+            puts 100 * noun + verb
+
+            exit
+        end
+    end
+end
