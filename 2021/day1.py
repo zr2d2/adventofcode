18f3d5e341dcbff9a118c5a6ca28696595d27552
Zach Rowe <zrowe007@gmail.com>
2021-12-22 18:33:51 -0500

    day 1 part 2

--- 2021/day1.py
+++ 2021/day1.py
@@ -1,10 +1,26 @@
+import pprint
+
+depths = []
+slices = []
+
+def chunks(lst, n):
+    sums = []
+    for i in range(0, len(lst)-n+1):
+        j=i+n
+        s = sum(lst[i:j])
+        sums.append(s)
+    return sums
+
 with open('day1input.txt') as f:
-    depths = f.readlines()
+    lines =f.readlines()
+    depths = [int(i.strip()) for i in lines]
+
+depths = list(chunks(depths, 3))
 
 inc = 0
 lastDepth = 0
 for depth in depths:
-    if int(depth) > lastDepth:
+    if depth > lastDepth:
         inc +=1
     lastDepth = int(depth)
 inc = inc - 1
