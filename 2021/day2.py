9cca93d57947703cbe206b48a70cfbfed55a03e1
Zach Rowe <zrowe007@gmail.com>
2021-12-24 19:43:23 -0500

    add part 2

--- 2021/day2.py
+++ 2021/day2.py
@@ -7,6 +7,7 @@ with open('day2input.txt') as f:
     lines = f.readlines()
     instructions = [i.split() for i in lines]
 
+#Part 1
 for inst in instructions:
     if (inst[0] == 'down'):
         depth = depth - int(inst[1])
@@ -19,4 +20,21 @@ for inst in instructions:
     print("after {}, you are at {},{}".format(inst, depth, horizonal))
 
 print("you are at {},{}".format(depth,horizonal))
+print("your multiple is {}".format(depth*horizonal))
+
+#Part 2
+depth = 0
+horizonal = 0
+aim = 0
+for inst in instructions:
+    if (inst[0] == 'down'):
+        aim = aim - int(inst[1])
+    elif (inst[0] == 'up'):
+        aim = aim + int(inst[1])
+    elif (inst[0] == 'forward'):
+        horizonal = horizonal + int(inst[1])
+        depth = depth - int(inst[1])*aim
+    else:
+        print("don't know how to process command {} with number {}".format(depth,horizonal))
+print("you are at {},{} with aim {}".format(depth, horizonal, aim))
 print("your multiple is {}".format(depth*horizonal))
