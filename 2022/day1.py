f5dfcb323af0672d8e930e11b2cca98bb0a78864
Zachary Rowe <zachary.rowe@ecstech.com>
2022-12-04 18:53:03 -0500

    day 1 part 2

--- 2022/day1.py
+++ 2022/day1.py
@@ -1,14 +1,14 @@
 with open('day1input.txt') as f:
     supplies = f.readlines()
 
-food = {}
+food = []
 most = 0
 sum  = 0
 elf = 0
 
 for entry in supplies:
   if entry == "\n":
-    food[elf] = sum 
+    food.append(sum)
     if sum > most:
       most = sum
     sum = 0
@@ -19,3 +19,6 @@ for entry in supplies:
     sum+= callories
 #print(food)
 print("the most food is {}".format(most))
+food.sort(reverse=True)
+sum = food[0] + food[1] + food[2]
+print("the top 3 elves are carying {} calories".format(sum))
